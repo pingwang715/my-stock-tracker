8a74122c93a6bb0fcabfbff64aaf658894697625
Ping Wang <pingwang715@gmail.com>
2026-03-26 09:51:11 +0100

    create frontend and backend

---
+++ MyStockTracker/src/main/java/com/wangping/MyStockTracker/MyStockTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.wangping.MyStockTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStockTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyStockTrackerApplication.class, args);
+	}
+
+}
---
+++ MyStockTracker/src/test/java/com/wangping/MyStockTracker/MyStockTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangping.MyStockTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStockTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
